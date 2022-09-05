5ef29596373c8dca55f30e8c3b595ecf52144576
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-09-05 16:07:33 +0200

    add footer header

--- src/components/FooterHeader.vue
+++ src/components/FooterHeader.vue
@@ -4,7 +4,50 @@
         <nav class="footer-nav">
 
             <ul class="footer-list">
-                
+                <li class="footer__list__item">
+                    <a href="#nogo">
+                        <img src="@/assets/buy-comics-digital-comics.png" alt="">
+                        <span>
+                            digital comics
+                        </span>
+                    </a>
+                </li>
+
+                <li class="footer__list__item">
+                    <a href="#nogo">
+                        <img src="@/assets/buy-comics-merchandise.png" alt="">
+                        <span>
+                            dc merchandise
+                        </span>
+                    </a>
+                </li>
+
+                <li class="footer__list__item">
+                    <a href="#nogo">
+                        <img src="@/assets/buy-comics-subscriptions.png" alt="">
+                        <span>
+                            subscription
+                        </span>
+                    </a>
+                </li>
+
+                <li class="footer__list__item">
+                    <a href="#nogo">
+                        <img src="@/assets/buy-comics-shop-locator.png" alt="">
+                        <span>
+                            comic shop locator
+                        </span>
+                    </a>
+                </li>
+
+                <li class="footer__list__item">
+                    <a href="#nogo">
+                        <img src="@/assets/buy-dc-power-visa.svg" alt="">
+                        <span>
+                            dc power visa
+                        </span>
+                    </a>
+                </li>
             </ul>
 
         </nav>
@@ -19,5 +62,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
+    @import '@/style/variables';
+    @import '@/style/mixins';
+    
+    .container {
+        background-color: $dc-blue;
+        color: #FFF;
+        padding: 3rem 1rem;
+        .footer-nav {
+            .footer-list {
+                @include list-wrapper;
+
+                .footer__list__item {
+                    @include list-items;
+
+                    a {
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        gap: 1rem;
+
+                        img {
+                            height: 50px;
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
 
 </style>
--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -85,6 +85,7 @@ export default {
 <style lang="scss" scoped>
 
     @import '@/style/variables';
+    @import '@/style/mixins';
 
     .container {
         display: flex;
